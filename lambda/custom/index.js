4dc9e61a7e415c2e0742b9aca1f01cb10f66c883
William Yen <williamyen0203@gmail.com>
2018-01-06 14:44:39 -0500

    Add YesIntent and NoIntent and bug fixes

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,34 +1,33 @@
-"use strict";
+'use strict';
 
-const Alexa = require("alexa-sdk");
+const Alexa = require('alexa-sdk');
 
 const GAME_STATES = {
-    START: "_STARTSTATE",
-    GAME: "_GAMESTATE",
-    REPLAY: "_REPLAYSTATE"
+    START: '_STARTSTATE',
+    GAME: '_GAMESTATE',
+    REPLAY: '_REPLAYSTATE'
 };
 
-const APP_NAME = "The Last Coin";
-// TODO
-const APP_ID = undefined;
+const APP_NAME = 'The Last Coin';
+const APP_ID = 'amzn1.ask.skill.c122c60f-50a5-420a-9944-b6b0bb97cd1f';
 
 const newSessionHandlers = {
     LaunchRequest: function () {
         this.handler.state = GAME_STATES.START;
-        this.emitWithState("StartGame", true);
+        this.emitWithState('StartGame', true);
     },
-    "AMAZON.StartOverIntent": function () {
+    'AMAZON.StartOverIntent': function () {
         this.handler.state = GAME_STATES.START;
-        this.emitWithState("StartGame", true);
+        this.emitWithState('StartGame', true);
     },
-    "AMAZON.HelpIntent": function () {
+    'AMAZON.HelpIntent': function () {
         this.handler.state = GAME_STATES.HELP;
-        this.emitWithState("helpTheUser", true);
+        this.emitWithState('helpTheUser', true);
     },
     Unhandled: function () {
-        const speechOutput = "Say start to start a new game.";
+        const speechOutput = 'Say start to start a new game.';
         this.response.speak(speechOutput).listen(speechOutput);
-        this.emit(":responseReady");
+        this.emit(':responseReady');
     }
 };
 
@@ -36,7 +35,7 @@ const coinsTotal = 30;
 
 const startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
     StartGame: function (newGame) {
-        let intro = newGame ? "Welcome to The Last Coin. " : "";
+        let intro = newGame ? 'Welcome to The Last Coin. ' : '';
         let reprompt = `There are ${coinsTotal} coins in the jar. If you can take the last coin, you win. You may take up to three coins at once. How many would you like to take?`;
 
         this.response.cardRenderer(APP_NAME, reprompt);
@@ -45,7 +44,7 @@ const startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
         Object.assign(this.attributes, {coinsTotal: coinsTotal, coinsLeft: coinsTotal});
 
         this.handler.state = GAME_STATES.GAME;
-        this.emit(":responseReady");
+        this.emit(':responseReady');
     }
 });
 
@@ -70,76 +69,92 @@ function calculateCoinsToTake() {
 }
 
 function handleUserGuess() {
-    const answerSlotValid = isAnswerSlotValid(this.event.request.intent);
+    const answerSlotValid = isAnswerSlotValid.call(this, this.event.request.intent);
     let speechOutput = '';
 
     if (answerSlotValid) {
-        const answer = parseInt(intent.slots.Answer.value, 10);
+        const answer = parseInt(this.event.request.intent.slots.Answer.value, 10);
         let coinsLeft = this.attributes['coinsLeft'];
 
         // check if user has won game
         if (answer === coinsLeft) {
-            speechOutput = "You won! Would you like to play again?";
+            speechOutput = 'Looks like you won! Would you like to play again?';
             this.response.cardRenderer(APP_NAME, speechOutput);
+            this.response.speak(speechOutput).listen(speechOutput);
 
             this.handler.state = GAME_STATES.REPLAY;
-            this.emitWithState("StartGame", false);
+            this.emit(':responseReady');
         } else {
             coinsLeft -= answer;
         }
 
         // check if Alexa has won the game
         if (coinsLeft <= 3) {
-            speechOutput = "Looks like I won! Would you like to play again?";
+            speechOutput = 'I take the last ';
+            speechOutput += (coinsLeft > 1) ? `${coinsLeft} coins. ` : 'coin. ';
+            speechOutput += 'Looks like I won! Would you like to play again?';
             this.response.cardRenderer(APP_NAME, speechOutput);
+            this.response.speak(speechOutput).listen(speechOutput);
 
             this.handler.state = GAME_STATES.REPLAY;
-            this.emitWithState("StartGame", false);
+            this.emit(':responseReady');
         } else {
             const coinsToTake = calculateCoinsToTake.call(this);
-            const currentCoins = coinsLeft - answer - coinsToTake;
+            const currentCoins = coinsLeft - coinsToTake;
 
             Object.assign(this.attributes, {
                 coinsTotal: coinsTotal,
                 coinsLeft: currentCoins
             });
-        }
 
-        speechOutput = `I take ${coinsToTake} coins.`;
-        this.response.cardRenderer(APP_NAME, speechOutput);
-        this.response.speak(speechOutput).listen(speechOutput);
+            speechOutput = `I take ${coinsToTake} coin`;
+            speechOutput += (coinsToTake > 1) ? 's. ' : '. ';
+            speechOutput += (currentCoins > 1) ? `There are now ${currentCoins} coins ` : 'There is now one coin ';
+            speechOutput += `left in the jar. How many coins would you like to take?`;
+            this.response.cardRenderer(APP_NAME, speechOutput);
+            this.response.speak(speechOutput).listen(speechOutput);
+        }
     } else {
-        speechOutput = `Please say a number between 1 and ${Math.min(this.attributes['coinsLeft'], 3)}.`;
+        if (this.attributes['coinsLeft'] === 1) {
+            speechOutput = 'There is only one coin left in the jar. How many coins do you want to take?';
+        } else {
+            speechOutput = `Please say a number between 1 and ${Math.min(this.attributes['coinsLeft'], 3)}.`;
+        }
 
         this.response.cardRenderer(APP_NAME, speechOutput);
         this.response.speak(speechOutput).listen(speechOutput);
     }
 
-    this.emit(":responseReady");
+    this.emit(':responseReady');
 }
 
 const gameStateHandlers = Alexa.CreateStateHandler(GAME_STATES.GAME, {
     AnswerIntent: function () {
         handleUserGuess.call(this);
     },
-    "AMAZON.StartOverIntent": function () {
+    'AMAZON.StartOverIntent': function () {
         this.handler.state = GAME_STATES.START;
-        this.emitWithState("StartGame", false);
+        this.emitWithState('StartGame', false);
     },
-    "AMAZON.RepeatIntent": function () {
+    'AMAZON.RepeatIntent': function () {
         this.response
             .speak(getPrompt(this.attributes['coinsLeft']))
             .listen(getPrompt(this.attributes['coinsLeft']));
-        this.emit(":responseReady");
+        this.emit(':responseReady');
     },
-    "AMAZON.StopIntent": function () {
-        this.response.speak("Thanks for playing! Come back soon!");
-        this.emit(":responseReady");
+    'AMAZON.StopIntent': function () {
+        this.response.speak('Thanks for playing! Come back soon!');
+        this.emit(':responseReady');
     },
     Unhandled: function () {
-        const speechOutput = `Please say a number between 1 and ${Math.min(this.attributes['coinsLeft'], 3)}.`;
+        let speechOutput = '';
+        if (this.attributes['coinsLeft'] === 1) {
+            speechOutput = 'There is only one coin left in the jar. How many coins do you want to take?';
+        } else {
+            speechOutput = `Please say a number between 1 and ${Math.min(this.attributes['coinsLeft'], 3)}.`;
+        }
         this.response.speak(speechOutput).listen(speechOutput);
-        this.emit(":responseReady");
+        this.emit(':responseReady');
     },
     SessionEndedRequest: function () {
         console.log(`Session ended in game state: ${this.event.request.reason}`);
@@ -147,18 +162,22 @@ const gameStateHandlers = Alexa.CreateStateHandler(GAME_STATES.GAME, {
 });
 
 const replayStateHandlers = Alexa.CreateStateHandler(GAME_STATES.REPLAY, {
-    "AMAZON.YesIntent": function () {
+    'YesIntent': function () {
         this.handler.state = GAME_STATES.START;
-        this.emitWithState("StartGame", false);
+        this.emitWithState('StartGame', false);
     },
-    "AMAZON.NoIntent": function () {
-        this.response.speak("Thanks for playing! Come back soon!");
-        this.emit(":responseReady");
+    'NoIntent': function () {
+        this.response.speak('Thanks for playing! Come back soon!');
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak('Thanks for playing! Come back soon!');
+        this.emit(':responseReady');
     },
     Unhandled: function () {
-        const speechOutput = "Please say yes or no.";
+        const speechOutput = 'Please say yes or no.';
         this.response.speak(speechOutput).listen(speechOutput);
-        this.emit(":responseReady");
+        this.emit(':responseReady');
     }
 });
 
