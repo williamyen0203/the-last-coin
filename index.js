3669e563ba523dded248772b5edf9c8e058edfd6
William Yen <williamyen0203@gmail.com>
2018-01-06 01:21:07 -0500

    Add main game loop

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const Alexa = require("alexa-sdk");
 
 const GAME_STATES = {
     START: '_STARTSTATE',
-    GAME: '_GAMESTATE'
+    GAME: '_GAMESTATE',
+    REPLAY: '_REPLAYSTATE'
 };
 
 const APP_NAME = 'The Last Coin';
@@ -31,10 +32,10 @@ const newSessionHandlers = {
     }
 };
 
+const coinsTotal = 30;
+
 const startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
     'StartGame': function (newGame) {
-        let coinsTotal = 30;
-
         let intro = newGame ? "Welcome to The Last Coin. " : '';
         let reprompt = `There are ${coinsTotal} coins in the jar. If you can take the last coin, you win. You may take up to three coins at once. How many would you like to take?`;
 
@@ -56,10 +57,71 @@ function getPrompt(coins) {
     return 'There are ${coins} coins in the jar. How many would you like to take?';
 }
 
+function isAnswerSlotValid(intent) {
+    const answerSlotFilled = intent && intent.slots && intent.slots.Answer && intent.slots.Answer.value;
+    const answerSlotIsInt = answerSlotFilled && !isNaN(parseInt(intent.slots.Answer.value, 10));
+    return answerSlotIsInt &&
+        parseInt(intent.slots.Answer.value, 10) <= Math.min(3, this.attributes['coinsLeft'] &&
+            parseInt(intent.slots.Answer.value, 10) >= 1);
+}
+
+function calculateCoinsToTake() {
+    return Math.min(Math.floor((Math.random() * 3) + 1), this.attributes['coinsLeft']);
+}
+
+function handleUserGuess() {
+    const answerSlotValid = isAnswerSlotValid(this.event.request.intent);
+
+    if (answerSlotValid) {
+        const answer = parseInt(intent.slots.Answer.value, 10);
+        let coinsLeft = this.attributes['coinsLeft'];
+
+        // check if user has won game
+        if (answer === coinsLeft) {
+            const speechOutput = 'You won! Would you like to play again?';
+            this.response.cardRenderer(APP_NAME, speechOutput);
+
+            this.handler.state = GAME_STATES.START;
+            this.emitWithState('StartGame', false);
+        } else {
+            coinsLeft = currentCoins - answer;
+        }
+
+        // check if Alexa has won the game
+        if (coinsLeft <= 3) {
+            const speechOutput = 'Looks like I won! Would you like to play again?';
+            this.response.cardRenderer(APP_NAME, speechOutput);
+
+            this.handler.state = GAME_STATES.START;
+            this.emitWithState('StartGame', false);
+        } else {
+            const coinsToTake = calculateCoinsToTake.call(this);
+            const currentCoins = coinsLeft - answer - coinsToTake;
+
+            Object.assign(this.attributes, {
+                'coinsTotal': coinsTotal,
+                'coinsLeft': currentCoins
+            })
+        };
+
+        const speechOutput = `I take ${coinsToTake} coins.`;
+        this.response.cardRenderer(APP_NAME, speechOutput);
+        this.response.speak(speechOutput)
+            .listen(speechOutput);
+    } else {
+        const speechOutput = `Please say a number between 1 and ${Math.min(this.attributes['coinsLeft'], 3)}.`;
+
+        this.response.cardRenderer(APP_NAME, speechOutput);
+        this.response.speak(speechOutput)
+            .listen(speechOutput);
+    }
+
+    this.emit(':responseReady');
+}
 
 const gameStateHandlers = Alexa.CreateStateHandler(GAME_STATES.GAME, {
     'AnswerIntent': function () {
-        handleUserGuess.call(this, false);
+        handleUserGuess.call(this);
     },
     'AMAZON.StartOverIntent': function () {
         this.handler.state = GAME_STATES.START;
@@ -74,7 +136,7 @@ const gameStateHandlers = Alexa.CreateStateHandler(GAME_STATES.GAME, {
         this.emit(':responseReady');
     },
     'Unhandled': function () {
-        const speechOutput = 'Say a number between 1 and 3.';
+        const speechOutput = `Please say a number between 1 and ${Math.min(this.attributes['coinsLeft'], 3)}.`;
         this.response.speak(speechOutput).listen(speechOutput);
         this.emit(':responseReady');
     },
