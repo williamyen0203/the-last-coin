02e538f13131f7ee486a6737314ba8257788e03b
William Yen <williamyen0203@gmail.com>
2018-01-08 20:04:05 -0500

    Add easy and hard difficulty

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -4,6 +4,7 @@ const Alexa = require('alexa-sdk');
 
 const GAME_STATES = {
     START: '_STARTSTATE',
+    SETUP: '_SETUPSTATE',
     GAME: '_GAMESTATE',
     REPLAY: '_REPLAYSTATE'
 };
@@ -15,6 +16,17 @@ const DIFFICULTY = {
     EASY: '_EASY',
     HARD: '_HARD'
 };
+
+const DIFFICULTY_CONFIG = {
+    EASY: {
+        COINS_TOTAL: 20,
+        COIN_LIMIT: 3
+    },
+    HARD: {
+        COINS_TOTAL: 40,
+        COIN_LIMIT: 5
+    }
+}
 
 const newSessionHandlers = {
     LaunchRequest: function () {
@@ -36,20 +48,47 @@ const newSessionHandlers = {
     }
 };
 
-const coinsTotal = 30;
-
 const startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
     StartGame: function (newGame) {
         let intro = newGame ? 'Welcome to The Last Coin. ' : '';
-        let reprompt = `There are ${coinsTotal} coins in the jar. If you can take the last coin, you win. You may take up to three coins at once. How many would you like to take?`;
+        intro += 'What difficulty would you like to play in? Easy or hard?';
 
-        this.response.cardRenderer(APP_NAME, reprompt);
-        this.response.speak(intro + reprompt).listen(reprompt);
+        this.response.cardRenderer(APP_NAME);
+        this.response.speak(intro).listen(intro);
+
+        this.handler.state = GAME_STATES.SETUP;
+        this.emit(':responseReady');
+    }
+});
+
+const setupStateHandlers = Alexa.CreateStateHandler(GAME_STATES.SETUP, {
+    EasyIntent: function() {
+        let prompt = `There are ${DIFFICULTY_CONFIG.EASY.COINS_TOTAL} coins in the jar. If you can take the last coin, you win. You may take up to ${DIFFICULTY_CONFIG.EASY.COIN_LIMIT} coins at once. How many would you like to take?`;
+
+        this.response.cardRenderer(APP_NAME, prompt);
+        this.response.speak(prompt).listen(prompt);
 
         Object.assign(this.attributes, {
-            coinsTotal: coinsTotal,
-            coinsLeft: coinsTotal,
+            coinsTotal: DIFFICULTY_CONFIG.EASY.COINS_TOTAL,
+            coinsLeft: DIFFICULTY_CONFIG.EASY.COINS_TOTAL,
+            coinLimit: DIFFICULTY_CONFIG.EASY.COIN_LIMIT,
             difficulty: DIFFICULTY.EASY
+        });
+
+        this.handler.state = GAME_STATES.GAME;
+        this.emit(':responseReady');
+    },
+    HardIntent: function() {
+        let prompt = `There are ${DIFFICULTY_CONFIG.HARD.COINS_TOTAL} coins in the jar. If you can take the last coin, you win. You may take up to ${DIFFICULTY_CONFIG.HARD.COIN_LIMIT} coins at once. How many would you like to take?`;
+
+        this.response.cardRenderer(APP_NAME, prompt);
+        this.response.speak(prompt).listen(prompt);
+
+        Object.assign(this.attributes, {
+            coinsTotal: DIFFICULTY_CONFIG.HARD.COINS_TOTAL,
+            coinsLeft: DIFFICULTY_CONFIG.HARD.COINS_TOTAL,
+            coinLimit: DIFFICULTY_CONFIG.HARD.COIN_LIMIT,
+            difficulty: DIFFICULTY.HARD
         });
 
         this.handler.state = GAME_STATES.GAME;
@@ -66,13 +105,13 @@ function isAnswerSlotValid(intent) {
     const answerSlotIsInt = answerSlotFilled && !isNaN(parseInt(intent.slots.Answer.value, 10));
     return (
         answerSlotIsInt &&
-        parseInt(intent.slots.Answer.value, 10) <= Math.min(3, this.attributes['coinsLeft']) && parseInt(intent.slots.Answer.value, 10) >= 1
+        parseInt(intent.slots.Answer.value, 10) <= Math.min(this.attributes['coinLimit'], this.attributes['coinsLeft']) && parseInt(intent.slots.Answer.value, 10) >= 1
     );
 }
 
 function calculateCoinsToTake() {
     return Math.min(
-        Math.floor(Math.random() * 3 + 1),
+        Math.floor(Math.random() * this.attributes['coinLimit'] + 1),
         this.attributes['coinsLeft']
     );
 }
@@ -97,8 +136,12 @@ function handleUserGuess() {
             coinsLeft -= answer;
         }
 
+        const coinsTotal = this.attributes['coinsTotal'];
+        const difficulty = this.attributes['difficulty'];
+        const coinLimit = this.attributes['coinLimit'];
+
         // check if Alexa has won the game
-        if (coinsLeft <= 3) {
+        if (coinsLeft <= coinLimit) {
             speechOutput = 'I take the last ';
             speechOutput += (coinsLeft > 1) ? `${coinsLeft} coins. ` : 'coin. ';
             speechOutput += 'Looks like I won! Would you like to play again?';
@@ -110,11 +153,11 @@ function handleUserGuess() {
         } else {
             const coinsToTake = calculateCoinsToTake.call(this);
             const currentCoins = coinsLeft - coinsToTake;
-
             Object.assign(this.attributes, {
                 coinsTotal: coinsTotal,
                 coinsLeft: currentCoins,
-                difficulty: DIFFICULTY.EASY
+                coinLimit: coinLimit,
+                difficulty: difficulty
             });
 
             speechOutput = `I take ${coinsToTake} coin`;
@@ -128,7 +171,7 @@ function handleUserGuess() {
         if (this.attributes['coinsLeft'] === 1) {
             speechOutput = 'There is only one coin left in the jar. How many coins do you want to take?';
         } else {
-            speechOutput = `Please say a number between 1 and ${Math.min(this.attributes['coinsLeft'], 3)}.`;
+            speechOutput = `Please say a number between 1 and ${Math.min(this.attributes['coinsLeft'], this.attributes['coinLimit'])}.`;
         }
 
         this.response.cardRenderer(APP_NAME, speechOutput);
@@ -161,7 +204,7 @@ const gameStateHandlers = Alexa.CreateStateHandler(GAME_STATES.GAME, {
         if (this.attributes['coinsLeft'] === 1) {
             speechOutput = 'There is only one coin left in the jar. How many coins do you want to take?';
         } else {
-            speechOutput = `Please say a number between 1 and ${Math.min(this.attributes['coinsLeft'], 3)}.`;
+            speechOutput = `Please say a number between 1 and ${Math.min(this.attributes['coinsLeft'], this.attributes['coinLimit'])}.`;
         }
         this.response.speak(speechOutput).listen(speechOutput);
         this.emit(':responseReady');
@@ -196,6 +239,7 @@ exports.handler = function (event, context) {
     alexa.appId = APP_ID;
     alexa.registerHandlers(
         newSessionHandlers,
+        setupStateHandlers,
         startStateHandlers,
         gameStateHandlers,
         replayStateHandlers
